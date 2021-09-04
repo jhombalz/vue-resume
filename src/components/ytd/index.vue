7b50a30391116ad4dd73aaff64b2ea18b7e1c95d
jhombalz <jhomar.baluca@gmail.com>
2021-09-04 15:02:56 +0800

    --update with ytd

--- src/components/ytd/index.vue
+++ src/components/ytd/index.vue
@@ -1,81 +1,13 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col cols="12" sm="12" md="5"> </v-col>
-
-      <v-col cols="12" sm="12" md="7">
-        <div>
-          <p>Youtube Downloader</p>
-        </div>
-
-        <div class="my-3">
-          <span class="text-disabled text-caption"> YOUTUBE URL / LINK: </span>
-          <input
-            class="border border-primary wd-100 pa-3 rounded text-primary"
-            v-model="url"
-            placeholder="https://www.youtube.com/watch?v=dUgNvsJnw2E"
-          />
-
-          <v-btn color="primary mt-2" @click="handleConvertUrl"> Go </v-btn>
-        </div>
-
-        <div class="mt-10">
-          <p v-for="(error, key) in errors" :key="key" class="text-error">
-            {{ error }}
-          </p>
-          <div v-if="downloadReady">
-            <span class="mr-5 text-h6"> {{ dowloadTitle }} </span> -
-            <a
-              :href="dowloadTag"
-              target="_blank"
-              download
-              class="text-secondary text-decoration-none text-subtitle-2 ml-6"
-            >
-              DOWLOAD
-            </a>
-          </div>
-        </div>
-      </v-col>
-    </v-row>
-  </v-container>
+  <search />
 </template>
 
 <script>
+import Search from "./search.vue";
 export default {
   name: "YoutubeDownloader",
-  data: () => ({
-    url: "",
-    dowloadTag: "",
-    dowloadTitle: "",
-    downloadReady: false,
-    errors: [],
-  }),
-
-  methods: {
-    handleConvertUrl: function () {
-      this.errors = [];
-      this.dowloadTag = "";
-      this.dowloadTitle = "";
-      this.downloadReady = false;
-
-      this.$http
-        .get(`/api/ytd/check-url`, {
-          params: {
-            url: this.url,
-          },
-        })
-        .then((response) => {
-          const data = response.data;
-          if (data.status) {
-            this.downloadReady = true;
-            this.dowloadTag = `${process.env.VUE_APP_API}/api/ytd/download-url?URL=${this.url}&downloadFormat=audio-only&title=${data.title}&quality=high`;
-            this.dowloadTitle = data.title;
-          } else {
-            this.errors.push(data.message);
-          }
-        })
-        .catch((error) => console.log("error: ", error));
-    },
+  components: {
+    search: Search,
   },
 };
 </script> 
---
+++ src/components/ytd/search.vue
@@ -0,0 +1,248 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="12">
+        <div>
+          <p class="text-h4">Youtube Downloader</p>
+        </div>
+
+        <div class="my-3">
+          <span class="text-disabled text-caption"> YOUTUBE Search: </span>
+          <input
+            class="border border-primary wd-100 pa-3 rounded text-primary"
+            v-model="url"
+            placeholder="Search or https://www.youtube.com/watch?v=dUgNvsJnw2E"
+          />
+
+          <div class="d-flex justify-end">
+            <v-btn
+              color="primary mt-2"
+              @click="searchVideo"
+              :disabled="loading.search"
+            >
+              <span v-if="loading.search"> Search... </span>
+              <span v-else> Search </span>
+            </v-btn>
+          </div>
+        </div>
+
+        <div class="mt-10">
+          <p v-for="(error, key) in errors" :key="key" class="text-error">
+            {{ error }}
+          </p>
+
+          <div v-if="searchData">
+            <div
+              class="my-7"
+              v-for="(item, key) in searchData.items"
+              :key="key"
+            >
+              <v-card>
+                <v-row>
+                  <v-col cols="12" sm="4" md="3" lg="2">
+                    <v-img
+                      :lazy-src="item.snippet.thumbnails.default.url"
+                      :src="item.snippet.thumbnails.default.url"
+                    >
+                    </v-img>
+                  </v-col>
+                  <v-col cols="12" sm="8" md="9" lg="10">
+                    <div class="pa-2">
+                      <p class="text-subtitle-1">{{ item.snippet.title }}</p>
+                      <p class="text-body-2 text-disabled">
+                        {{ item.snippet.description }}
+                      </p>
+                    </div>
+                    <div class="pa-2">
+                      <v-btn
+                        variant="text"
+                        color="secondary"
+                        @click="handleDownloadMusic($event, item.id.videoId)"
+                        :disabled="
+                          item.id.videoId === downloadVideoId && loading.mp3
+                        "
+                      >
+                        <span
+                          v-if="
+                            item.id.videoId === downloadVideoId && loading.mp3
+                          "
+                        >
+                          Generate MP3 ...
+                        </span>
+                        <span v-else> Generate MP3 </span>
+                      </v-btn>
+
+                      <v-btn
+                        variant="text"
+                        color="primary"
+                        @click="handleDownloadVideo($event, item.id.videoId)"
+                        :disabled="
+                          item.id.videoId === downloadVideoId && loading.video
+                        "
+                      >
+                        <span
+                          v-if="
+                            item.id.videoId === downloadVideoId && loading.video
+                          "
+                        >
+                          Generate Video ...
+                        </span>
+                        <span v-else> Generate Video </span>
+                      </v-btn>
+
+                      <div
+                        class="pa-2"
+                        v-bind:style="
+                          item.id.videoId === downloadVideoId && downloadReady
+                            ? 'display: block'
+                            : 'display: none'
+                        "
+                      >
+                        <v-btn
+                          color="primary"
+                          variant="contained"
+                          @click="handleDownloadFile"
+                        >
+                          {{ downloadSize }} MB - DOWNLOAD
+                        </v-btn>
+                      </div>
+                    </div>
+
+                    <!-- trigger -->
+                    <a
+                      ref="musicTag"
+                      target="_blank"
+                      download
+                      class="text-subtitle-2"
+                    />
+                  </v-col>
+                </v-row>
+              </v-card>
+            </div>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "YoutubeDownloader",
+  data: () => ({
+    url: "",
+    dowloadTag: "",
+    dowloadTitle: "",
+    downloadReady: false,
+    errors: [],
+    searchData: null,
+    downloadSize: 0,
+    downloadVideoId: null,
+    loading: {
+      search: false,
+      mp3: false,
+      video: false,
+    },
+  }),
+
+  methods: {
+    searchVideo: function () {
+      this.errors = [];
+      this.searchData = null;
+      if (this.url) {
+        this.loading.search = true;
+        this.$http
+          .get(`/api/ytd/search`, {
+            params: {
+              search: this.url,
+            },
+          })
+          .then((response) => {
+            const result = response.data;
+            this.searchData = result.data;
+          })
+          .catch((error) => console.log("error: ", error))
+          .finally(() => (this.loading.search = false));
+      } else {
+        this.errors.push("Search field is required.");
+      }
+    },
+
+    handleDownloadMusic: function (event, videoId) {
+      this.dowloadTag = "";
+      this.downloadReady = false;
+      this.downloadVideoId = videoId;
+
+      var xuri = `https://www.youtube.com/watch?v=${videoId}`;
+      this.loading.mp3 = true;
+
+      this.$http
+        .get(`/api/ytd/check-url`, {
+          params: {
+            url: xuri,
+            type: "music",
+          },
+        })
+        .then((response) => {
+          const data = response.data;
+          if (data.status) {
+            this.$refs.musicTag.href = `${process.env.VUE_APP_API}/api/ytd/download-url-mp3?URL=${xuri}&downloadFormat=audioonly&title=${data.title}&quality=highestaudio`;
+
+            this.downloadReady = true;
+
+            this.downloadSize = (
+              Math.round(parseInt(data.audioFormats[0].contentLength) / 1000) /
+              1000
+            ).toFixed(2);
+          } else {
+            this.errors.push(data.message);
+          }
+        })
+        .catch((error) => console.log("error: ", error))
+        .finally(() => (this.loading.mp3 = false));
+    },
+
+    handleDownloadVideo: function (event, videoId) {
+      this.dowloadTag = "";
+      this.downloadReady = false;
+
+      var xuri = `https://www.youtube.com/watch?v=${videoId}`;
+      this.downloadVideoId = videoId;
+      this.loading.video = true;
+
+      this.$http
+        .get(`/api/ytd/check-url`, {
+          params: {
+            url: xuri,
+            type: "video",
+          },
+        })
+        .then((response) => {
+          const data = response.data;
+          if (data.status) {
+            this.$refs.musicTag.href = `${process.env.VUE_APP_API}/api/ytd/download-url-video?URL=${xuri}&downloadFormat=audioandvideo&title=${data.title}&quality=highestvideo`;
+            this.downloadReady = true;
+            this.downloadSize = (
+              Math.round(parseInt(data.audioFormats[0].contentLength) / 1000) /
+              1000
+            ).toFixed(2);
+          } else {
+            this.errors.push(data.message);
+          }
+        })
+        .catch((error) => console.log("error: ", error))
+        .finally(() => (this.loading.video = false));
+    },
+
+    handleDownloadFile: function () {
+      this.$refs.musicTag.click();
+    },
+  },
+};
+</script> 
+
+<style scoped>
+.wd-100 {
+  width: 100%;
+}
+</style>
